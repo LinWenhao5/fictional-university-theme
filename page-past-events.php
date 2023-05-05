a9fd93df979b0ae8f8e91406c1c1617fe454f45c
Lin Wenhao <linwenhao5@gmail.com>
2023-05-05 16:24:25 +0200

    day3

--- page-past-events.php
+++ page-past-events.php
@@ -4,18 +4,34 @@ get_header();
   <div class="page-banner">
     <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('images/ocean.jpg') ?>);"></div>
     <div class="page-banner__content container container--narrow">
-      <h1 class="page-banner__title">All Event</h1>
+      <h1 class="page-banner__title">Past Events</h1>
       <div class="page-banner__intro">
-        <p>See what is going on in our world</p>
+        <p>A recape of our past events.</p>
       </div>
     </div>  
   </div>
 
   <div class="container container--narrow page-section">
     <?php
-      while(have_posts())
+    $today = date('Ymd');
+    $pastEvents = new WP_Query(array(
+        'paged' => get_query_var('paged', 1),
+        'post_type' => 'event',
+        'meta_key' => 'event_date',
+        'orderby' => 'meta_value_num',
+        'order' => 'ASC',
+        'meta_query' => array(
+            array(
+                'key' => 'event_date',
+                'compare' => '<=',
+                'value' => $today,
+                'type' => 'numric'
+                )
+            )
+        ));
+      while($pastEvents->have_posts())
       {
-        the_post();
+        $pastEvents->the_post();
       ?>
         <div class="event-summary">
           <a class="event-summary__date t-center" href="<?php the_permalink() ?>">
@@ -40,9 +56,9 @@ get_header();
     <?php  
       }
     ?>
-    <?php echo paginate_links() ?>
-    <hr class="section-break">
-    <p>Looking for a recap of past events? <a href="<?php echo site_url('past-event')?>">check out our past event archive</a></p>
+    <?php echo paginate_links(array(
+        'total' => $pastEvents->max_num_pages,
+    )) ?>
   </div>
 <?php
 get_footer();
